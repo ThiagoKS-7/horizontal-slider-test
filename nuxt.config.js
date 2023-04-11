a859f6f4b1240171da42ea24898127dc219c90f2
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-04-11 07:15:49 -0300

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,115 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'slider-test',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+   // Global CSS: https://go.nuxtjs.dev/config-css
+   css: [
+    '@/static/css/main.css',
+  ],
+  scss: [
+    '@/static/scss/global.scss',
+    '@/static/scss/varibles.scss',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    "@nuxtjs/color-mode",
+    "@nuxtjs/axios",
+    '@nuxtjs/composition-api/module',
+    '@pinia/nuxt',
+    '@nuxtjs/style-resources',
+  ],
+  axios: {
+    retry: { retries: 3 },
+    credentials: true,
+    baseURL: process.env.BASE_URL, // Used as fallback if no runtime config is provided
+    headers: {
+      common: {
+        'Accept': 'application/json, text/plain, */*'
+      },
+      delete: {},
+      get: {},
+      head: {},
+      post: {},
+      put: {},
+      patch: {}
+    }
+  },
+
+  publicRuntimeConfig: {
+    axios: {
+      browserBaseURL: process.env.BROWSER_BASE_URL
+    }
+  },
+
+  privateRuntimeConfig: {
+    axios: {
+      baseURL: process.env.BASE_URL
+    }
+  },
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storageKey: 'nuxt-color-mode'
+  },
+
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@pinia/nuxt',
+  ],
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      'defineStore', // import { defineStore } from 'pinia'
+      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    loaders: {
+      sass: {
+        implementation: require('sass'),
+      },
+      scss: {
+        implementation: require('sass'),
+      },
+    },
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          'tailwindcss/nesting': 'postcss-nesting',
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  }
+}
